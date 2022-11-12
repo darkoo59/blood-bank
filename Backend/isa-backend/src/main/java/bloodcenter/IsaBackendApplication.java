7989eac784157c97096ab4ab51f4a626b23837c3
darkoo59 <darko.selakovic11@gmail.com>
2022-11-12 02:39:20 +0100

    Added publisher functionality for hospital news

--- Backend/isa-backend/src/main/java/bloodcenter/IsaBackendApplication.java
+++ Backend/isa-backend/src/main/java/bloodcenter/IsaBackendApplication.java
@@ -1,11 +1,17 @@
 package bloodcenter;
 
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class IsaBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(IsaBackendApplication.class, args);
     }
