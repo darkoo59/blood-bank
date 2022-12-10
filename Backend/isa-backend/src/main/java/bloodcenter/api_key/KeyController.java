28a843fac77ee00dff43932e770ebd071982f012
Rade Stojanovic <stojanovicrade614@gmail.com>
2022-12-10 20:41:08 +0100

    api key implementation

--- Backend/isa-backend/src/main/java/bloodcenter/api_key/KeyController.java
+++ Backend/isa-backend/src/main/java/bloodcenter/api_key/KeyController.java
@@ -1,17 +1,36 @@
 package bloodcenter.api_key;
 
+import bloodcenter.core.ErrorResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
 
 @RestController
 @RequestMapping("api/key")
 public class KeyController {
-
+    private final KeyService keyService;
     @Autowired
-    private KeyService keyService;
+    public KeyController(KeyService keyService){
+        this.keyService = keyService;
+    }
 
     @PostMapping("/create")
-    public String createKey(@RequestBody String email) {
-        return "6c66af456eaf";
+    public String createKey(@RequestBody String email) throws Key.ApiKeyAlreadyExistsException {
+        return keyService.createKey(email);
+    }
+
+    @ExceptionHandler({ Key.ApiKeyAlreadyExistsException.class })
+    public ResponseEntity<Object> handleKeyExceptions(Exception ex){
+        ex.printStackTrace();
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({ Exception.class })
+    public ResponseEntity<Object> handleExceptions(Exception ex){
+        ex.printStackTrace();
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage()), HttpStatus.UNAUTHORIZED);
     }
 }
--- Backend/isa-backend/src/main/java/bloodcenter/api_key/KeyService.java
+++ Backend/isa-backend/src/main/java/bloodcenter/api_key/KeyService.java
@@ -3,9 +3,46 @@ package bloodcenter.api_key;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
 @Service
 public class KeyService {
-
+    private final KeyRepository keyRepository;
     @Autowired
-    private KeyRepository keyRepository;
+    public KeyService (KeyRepository keyRepository){
+        this.keyRepository = keyRepository;
+    }
+
+    public String createKey(String email) throws Key.ApiKeyAlreadyExistsException {
+        if(keyRepository.findByEmail(email) != null)
+            throw new Key.ApiKeyAlreadyExistsException("Key for the given email already exists.");
+
+        Key key = new Key();
+        key.setEmail(email);
+        key.setKey(generateRandomString());
+
+        keyRepository.save(key);
+
+        return key.getKey();
+    }
+
+    public boolean isKeyValid(String email, String apiKey){
+        Key key = keyRepository.findByEmail(email);
+        String[] arr = apiKey.split(" ");
+
+        return arr.length == 2 && key != null && arr[1].equals(key.getKey());
+    }
+
+
+    private String generateRandomString() {
+        int leftLimit = 97;
+        int rightLimit = 122;
+        int targetStringLength = 10;
+        Random random = new Random();
+
+        return random.ints(leftLimit, rightLimit + 1)
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+    }
 }
